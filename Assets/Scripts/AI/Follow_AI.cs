c040701ebdf0d343387aa4fb06396e84023783bc
Eduard Yunusov <yunusov_eduard@inbox.ru>
2023-11-09 02:24:51 +0300

    Script editing
    - Added a few Enemy Type.
    - Added another variant to start a battle scene

--- Assets/Scripts/AI/Follow_AI.cs
+++ Assets/Scripts/AI/Follow_AI.cs
@@ -3,14 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EnemyType
+{
+    Melee_Type,
+    Range_Type
+}
 
 
 public class Follow_AI : MonoBehaviour
 {
+    public EnemyType enemyType;
+
+    [Header("Enemy Settings")]
+
     [SerializeField] private float m_Speed;
     [SerializeField] private Transform m_Target;
     [SerializeField] private float m_MinDist;
     [SerializeField] private float m_ShootingRange;
+    [SerializeField] private float m_MeleeTriggerRange;
+    
+    [Header("Projectile and shooting system settings")]
     [SerializeField] private Projectile m_Projectile;
     [SerializeField] private GameObject m_ParentProjectile;
     [SerializeField] private float m_FireRate;
@@ -28,17 +40,34 @@ public class Follow_AI : MonoBehaviour
 
     private void Update()
     {
-        float distanceFromPlayer = Vector2.Distance(transform.position, m_Target.position);
-        if (distanceFromPlayer < m_MinDist && distanceFromPlayer > m_ShootingRange)
+        if (enemyType == EnemyType.Range_Type)
         {
-            transform.position = Vector2.MoveTowards(transform.position, m_Target.position, m_Speed * Time.deltaTime);
+            float distanceFromPlayer = Vector2.Distance(transform.position, m_Target.position);
+            if (distanceFromPlayer < m_MinDist && distanceFromPlayer > m_ShootingRange)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, m_Target.position, m_Speed * Time.deltaTime);
+            }
+            else if (distanceFromPlayer <= m_ShootingRange && m_NextShotTime < Time.time)
+            {
+                Projectile currentProjectile = Instantiate(m_Projectile, m_ParentProjectile.transform.position, Quaternion.identity);
+                m_NextShotTime = Time.time + m_FireRate;
+                currentProjectile.SetFollow_AI(this);
+            }
         }
-        else if (distanceFromPlayer <= m_ShootingRange && m_NextShotTime < Time.time)
+
+        if (enemyType == EnemyType.Melee_Type)
         {
-            Projectile currentProjectile = Instantiate(m_Projectile, m_ParentProjectile.transform.position, Quaternion.identity);
-            m_NextShotTime = Time.time + m_FireRate;
-            currentProjectile.SetFollow_AI(this);
+            float distanceFromPlayer = Vector2.Distance(transform.position, m_Target.position);
+            if (distanceFromPlayer < m_MinDist && distanceFromPlayer > m_MeleeTriggerRange)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, m_Target.position, m_Speed * Time.deltaTime);
+            }
+            else if (distanceFromPlayer <= m_MeleeTriggerRange)
+            {
+                EnableBattleScene();
+            }
         }
+        
     }
 
     private void OnDrawGizmosSelected()
